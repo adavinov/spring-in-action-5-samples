e5239a2a16c515d691140a7f453852bf0cfcfea1
woodapiary <git@woodapiary.com>
2020-11-11 21:16:48 +0300

    up ch5 to 2.3.5

--- ch05/tacos/src/main/java/tacos/security/SecurityConfig.java
+++ ch05/tacos/src/main/java/tacos/security/SecurityConfig.java
@@ -1,18 +1,19 @@
 package tacos.security;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 import org.springframework.security.core.userdetails.UserDetailsService;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.crypto.password.StandardPasswordEncoder;
 
 @Configuration
 @EnableWebSecurity
+@SuppressWarnings("deprecation")
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Autowired
@@ -49,7 +50,6 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     public PasswordEncoder encoder() {
         return new StandardPasswordEncoder("53cr3t");
     }
-
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth)
--- ch05/tacos/src/main/java/tacos/web/OrderProps.java
+++ ch05/tacos/src/main/java/tacos/web/OrderProps.java
@@ -1,5 +1,4 @@
 
-
 package tacos.web;
 
 import javax.validation.constraints.Max;
@@ -13,11 +12,8 @@ import org.springframework.validation.annotation.Validated;
 
 @Component
 @ConfigurationProperties(prefix = "taco.orders")
-
 @Validated
-
 public class OrderProps {
-
 
     @Min(value = 5, message = "must be between 5 and 25")
     @Max(value = 25, message = "must be between 5 and 25")
